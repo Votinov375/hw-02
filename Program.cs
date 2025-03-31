e439ff0cd88c4482ba1942f5d7756229f5743dfb
Votinov375 <Votinov34@gmail.com>
2025-03-31 14:25:18 +0300

    homework done

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿bool a = true;
+Console.WriteLine("Type: " + typeof(bool) + ", Example: " + a + ", Size: " + sizeof(bool));
+
+sbyte b = -128;
+Console.WriteLine("Type: " + typeof(sbyte) + ", Example: " + b + ", Size: " + sizeof(sbyte));
+
+byte c = 255;
+Console.WriteLine("Type: " + typeof(byte) + ", Example: " + c + ", Size: " + sizeof(byte));
+
+short d = -10000;
+Console.WriteLine("Type: " + typeof(short) + ", Example: " + d + ", Size: " + sizeof(short));
+
+int e = 1000000000;
+Console.WriteLine("Type: " + typeof(int) + ", Example: " + e + ", Size: " + sizeof(int));
+
+uint f = 0;
+Console.WriteLine("Type: " + typeof(uint) + ", Example: " + f + ", Size: " + sizeof(uint));
+
+long g = 1000000000000000000;
+Console.WriteLine("Type: " + typeof(long) + ", Example: " + g + ", Size: " + sizeof(long));
+
+ushort h = 10000;
+Console.WriteLine("Type: " + typeof(ushort) + ", Example: " + h + ", Size: " + sizeof(ushort));
+
+ulong i = 20;
+Console.WriteLine("Type: " + typeof(ulong) + ", Example: " + i + ", Size: " + sizeof(ulong));
+
+float j = 123.45f;
+Console.WriteLine("Type: " + typeof(float) + ", Example: " + j + ", Size: " + sizeof(float));
+
+double k = 123.45;
+Console.WriteLine("Type: " + typeof(double) + ", Example: " + k + ", Size: " + sizeof(double));
+
+char l = 'a';
+Console.WriteLine("Type: " + typeof(char) + ", Example: " + l + ", Size: " + sizeof(char));
+
+decimal m = 246.80m;
+Console.WriteLine("Type: " + typeof(decimal) + ", Example: " + m + ", Size: " + sizeof(decimal));
+
+string n = "Belhard";
+Console.WriteLine("Type: " + typeof(string) + ", Example: " + n + ", Size: " + "limited by computer resourses");
+
